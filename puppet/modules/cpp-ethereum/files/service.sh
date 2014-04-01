f0ef29a7e966105225bc0009bd61ab7d6eddc7b9
zelig <viktor.tron@gmail.com>
2014-04-01 16:30:42 +0700

    cleanup

--- puppet/modules/cpp-ethereum/files/service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ETHHOME=/home/ethereum/
-LOG=${ETHHOME}/eth.log
-ETH=/usr/local/bin/eth
-while [ 1 ]; do
-  if [ -f $LOG ] && sudo -u ethereum mv $LOG $LOG-\$(date +"%Y%m%d-%H$%M%S")
-  sudo -u ethereum $ETH -o peer -x 256 -l 30303 -m on -v 1 > $LOG
-done
